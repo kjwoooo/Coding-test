2c68ac38f0c60237881d784b96ffb2836cd40b90
권재우 <121236827+kjwoooo@users.noreply.github.com>
2024-01-02 14:53:30 +0900

    [level unrated] Title: 편지, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        
+         int strLength = message.length();
+        int answer = strLength* 2;
+        return answer;
+    }
+}
